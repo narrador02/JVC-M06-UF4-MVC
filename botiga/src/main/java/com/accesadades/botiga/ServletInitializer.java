9e27b04574a59bc715151bfa0c4ddc67bedbc462
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-03-23 23:16:09 +0100

    Empezado

---
+++ botiga/src/main/java/com/accesadades/botiga/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.accesadades.botiga;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BotigaApplication.class);
+	}
+
+}
